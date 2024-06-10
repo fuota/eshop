edf9db5e036c127c43e74c82432c12f5a8dc5a17
Tammy Do <103426061+fuota@users.noreply.github.com>
2024-06-10 11:31:40 -0500

    Add register view

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='register'),
 
-    path('', views.getRoutes, name="routes" ),
+
     path('users/profile/', views.getUserProfile, name="user-profile"),
     path('users/', views.getUsers, name="users"),
 
