d01dc1129e39fc05451d2f58c9885438d7383a4c
Tammy Do <103426061+fuota@users.noreply.github.com>
2024-06-10 11:51:27 -0500

    Add signal to update username according to email

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,7 +83,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
-    
+
     "TOKEN_OBTAIN_SERIALIZER": "my_app.serializers.MyTokenObtainPairSerializer",
     "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
     "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
