dc1d8a16fe9860039e0778f01575dd94c2fbc22e
Tammy Do <103426061+fuota@users.noreply.github.com>
2024-06-11 05:12:56 -0500

    Add login, logout, register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Footer from './components/Footer.js';
 import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from './screens/ProductScreen.js';
 import CartScreen from './screens/CartScreen.js';
+import LoginScreen from './screens/LoginScreen.js';
+import RegisterScreen from './screens/RegisterScreen.js';
+
 
 
 function App() {
@@ -14,6 +17,8 @@ function App() {
       <main className='py-3'>
         <Container>
           <Routes>
+            <Route path='/login' element={<LoginScreen/>}/>
+            <Route path='/register' element={<RegisterScreen/>}/>
             <Route path='/' element={<HomeScreen/>} exact/> //just render the HomeScreen component when the path is '/'
             <Route path='/product/:id' element={<ProductScreen/>}/> //just render the HomeScreen component when the path is '/'
             <Route path='/cart/:id?' element={<CartScreen/>}/>
