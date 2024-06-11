f23c22f22222484b1b72da3ec635f020524e79c8
Tammy Do <103426061+fuota@users.noreply.github.com>
2024-06-11 05:06:19 -0500

    Change error response

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,8 +20,8 @@ export const listProducts = () => async (dispatch) => {
     catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL, 
-            payload: error.response && error.response.data.message 
-                ? error.response.data.message 
+            payload: error.response && error.response.data.detail 
+                ? error.response.data.detail 
                 : error.message
         })
     }
@@ -40,8 +40,8 @@ export const listProductsDetails = (id) => async (dispatch) => {
     catch (error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL, 
-            payload: error.response && error.response.data.message 
-                ? error.response.data.message 
+            payload: error.response && error.response.data.detail 
+                ? error.response.data.detail 
                 : error.message
         })
     }
